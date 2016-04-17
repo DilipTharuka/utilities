0b92ed8815e072b4d9acfd91bd1a81490f49c17d
DilipTharuka <diliptharuka@gmail.com>
2016-04-17 12:11:14 +0530

    add more fertures

---
+++ start orient and tomcat.bat
@@ -0,0 +1,5 @@
+cd "C:\Softwares\orientdb\bin"
+start "orientDb" "C:\Softwares\orientdb\bin\server.bat"
+TIMEOUT 3
+cd "C:\Softwares\apache-tomcat-8.0.18\bin"
+start startup.bat
---
+++ stop orient and tomcat.bat
@@ -0,0 +1,5 @@
+cd "C:\Softwares\orientdb\bin"
+start "orientDb" "C:\Softwares\orientdb\bin\shutdwon.bat"
+TIMEOUT 3
+cd "C:\Softwares\apache-tomcat-8.0.20\bin"
+start shutdown.bat
